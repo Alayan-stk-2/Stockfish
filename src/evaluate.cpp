5f9a26b6c727cfc354d593bb26c5219f020b5763
Alayan <alayan.feh@gmail.com>
2018-12-28 21:46:37 +0100

    Remove code from another idea

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -158,7 +158,6 @@ namespace {
   constexpr Score Hanging            = S( 69, 36);
   constexpr Score KingProtector      = S(  7,  8);
   constexpr Score KnightOnQueen      = S( 16, 12);
-  constexpr Score KnightOnPasser     = S(  0, 10);
   constexpr Score LongDiagonalBishop = S( 45,  0);
   constexpr Score MinorBehindPawn    = S( 18,  3);
   constexpr Score PawnlessFlank      = S( 17, 95);
