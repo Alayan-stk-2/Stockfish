827b6f0b25825ab2e3683d88e28aa03aae326c87
Alayan <alayan.feh@gmail.com>
2018-12-28 21:47:30 +0100

    Remove code from another idea

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -339,10 +339,9 @@ namespace {
                 // bishop, bigger when the center files are blocked with pawns.
                 Bitboard blocked = pos.pieces(Us, PAWN) & shift<Down>(pos.pieces());
 
-                blocked = (DarkSquares & s) ? DarkSquares & blocked : ~DarkSquares & blocked;
-
-                score -= BishopPawns * popcount(blocked)
+                score -= BishopPawns * pe->pawns_on_same_color_squares(Us, s)
                                      * (1 + popcount(blocked & CenterFiles));
+
                 // Bonus for bishop on a long diagonal which can "see" both center squares
                 if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))
                     score += LongDiagonalBishop;
