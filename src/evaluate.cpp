8f97b5bf5bf63a8d1dd012b3677332e5545fd168
Alayan <alayan.feh@gmail.com>
2018-12-29 15:50:14 +0100

    With parameter tweak

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -153,7 +153,7 @@ namespace {
 
   // Assorted bonuses and penalties
   constexpr Score BishopPawns        = S(  3,  7);
-  constexpr Score BishopPawns2       = S(  9,  6);
+  constexpr Score BishopPawns2       = S( 11,  5);
   constexpr Score CloseEnemies       = S(  8,  0);
   constexpr Score CorneredBishop     = S( 50, 50);
   constexpr Score Hanging            = S( 69, 36);
