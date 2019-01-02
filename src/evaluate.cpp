3eae0b5ba7251b39e2766fcd1483e90e99ea7a26
Alayan <alayan.feh@gmail.com>
2019-01-02 18:49:24 +0100

    Bonus at 15, 30

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -683,7 +683,7 @@ namespace {
                 bool defended_pawn = (SquareBB[s] & (~attackedBy[Them][ALL_PIECES] | (~attackedBy2[Them] & attackedBy[Us][ALL_PIECES])));
 
                 if (defended_pawn && b_sac)
-                    bonus += make_score(25, 50);
+                    bonus += make_score(15, 30);
             }
         } // rank > RANK_3
 
