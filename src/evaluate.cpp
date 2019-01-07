91b369d9ae71cf5e86001c94afbfc9d9934cd01e
Alayan <alayan.feh@gmail.com>
2019-01-07 22:08:32 +0100

    Fix character

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -510,7 +510,7 @@ namespace {
     // Squares strongly protected by the enemy, either because they defend the
     // square with a pawn, or because they defend the square twice and we don't.
     stronglyProtected =  attackedBy[Them][PAWN]
-                       | ((attackedBy2[Them] | (attackedBy[Them][ALL_PIECES] & attackedBy[Us][QUEEN])) & ~attackedBy2[Us]);
+                       | ((attackedBy2[Them] | (attackedBy[Them][ALL_PIECES] & attackedBy[Us][QUEEN])) & ~attackedBy2[Us]);
 
     // Non-pawn enemies, strongly protected
     defended = nonPawnEnemies & stronglyProtected;
