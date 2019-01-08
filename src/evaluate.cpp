38ce04a61258b0e938e45f05fdf9abd8af9f4fd7
Alayan <alayan.feh@gmail.com>
2019-01-08 22:56:36 +0100

    Extra malus for very weak square but smaller malus otherwise

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -466,7 +466,8 @@ namespace {
 
     kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                  +  69 * kingAttacksCount[Them]
-                 + 185 * popcount(kingRing[Us] & weak)
+                 + 175 * popcount(kingRing[Us] & weak)
+                 +  50 * popcount(kingRing[Us] & weak & attackedBy2[Them])
                  + 150 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
                  +       tropism * tropism / 4
                  - 873 * !pos.count<QUEEN>(Them)
