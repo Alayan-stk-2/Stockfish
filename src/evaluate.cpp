69c30072ad1b5cdd043b4d0c76a30faab9ca346d
Alayan <alayan.feh@gmail.com>
2019-01-08 15:53:21 +0100

    Use results of tuning

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -671,18 +671,14 @@ namespace {
                 bonus += make_score(k * w, k * w);
             }
             // If the passed pawn is on rank 7, a 8th rank sac is often winning
+            // This code gives a bonus in situations where such a sac can be threatened
             else if (r == RANK_7 && (pos.count<ROOK>(Us) + pos.count<QUEEN>(Us)))
             {
-
-                Bitboard b_sac = pawn_attacks_bb<Us>(SquareBB[s]);
-
-                b_sac &= (attackedBy[Us][ROOK] | attackedBy[Us][QUEEN]);
-                b_sac &= ~attackedBy2[Them];
-
-                bool defended_pawn = (SquareBB[s] & (~attackedBy[Them][ALL_PIECES] | (~attackedBy2[Them] & (attackedBy[Us][BISHOP] | attackedBy[Us][KNIGHT]))));
-
-                if (defended_pawn)
-                    bonus += b_sac ? make_score(150, 200) : make_score(30, 60);
+                if (  SquareBB[s]
+                    & (   ~attackedBy[Them][ALL_PIECES]
+                        | (~attackedBy2[Them] & (attackedBy[Us][BISHOP] | attackedBy[Us][KNIGHT]))
+                        | (attackedBy2[Them] & attackedBy2[Us])))
+                    bonus += make_score(112, 59);
             }
         } // rank > RANK_3
 
