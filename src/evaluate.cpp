31e5efa318e90453245789156461a673f990abe4
Alayan <alayan.feh@gmail.com>
2019-12-11 03:20:56 +0100

    Take 1

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -705,6 +705,8 @@ namespace {
     int outflanking =  distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK))
                      - distance<Rank>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
 
+    outflanking = std::max(-2, outflanking);
+
     bool pawnsOnBothFlanks =   (pos.pieces(PAWN) & QueenSide)
                             && (pos.pieces(PAWN) & KingSide);
 
