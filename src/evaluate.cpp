b6df0b7321afe115b82b2644da4a7db20eea0352
Alayan <alayan.feh@gmail.com>
2020-02-13 22:47:36 +0100

    Value 12

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -755,7 +755,7 @@ namespace {
             && pos.non_pawn_material() == 2 * BishopValueMg)
             sf = 22 ;
         else
-            sf = std::min(sf, 36 - 20 * loneQueen + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide));
+            sf = std::min(sf, 36 - 12 * loneQueen + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide));
 
         sf = std::max(0, sf - (pos.rule50_count() - 12) / 4);
     }
